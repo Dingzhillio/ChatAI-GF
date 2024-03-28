e9d074004edcf20e9634fbb22031152a19a032df
emdi <emdi85.off@gmail.com>
2024-03-28 08:42:28 -0500

    images

--- src/Images.js
+++ src/Images.js
@@ -1,62 +1,62 @@
 export const IMAGES = [
     {
         id: 0,
-        url: "images/64263219044.png"
+        url: `${process.env.REACT_APP_FRONT}images/64263219044.png`
     },
     {
         id: 1,
-        url: "images/Background_04.png"
+        url: `${process.env.REACT_APP_FRONT}images/Background_04.png`
     },
     {
         id: 2,
-        url: "images/background_landing.png"
+        url: `${process.env.REACT_APP_FRONT}images/background_landing.png`
     },
     {
         id: 3,
-        url: "images/boy-eye.png"
+        url: `${process.env.REACT_APP_FRONT}images/boy-eye.png`
     },
     {
         id: 4,
-        url: "images/boy.png"
+        url: `${process.env.REACT_APP_FRONT}images/boy.png`
     },
     {
         id: 5,
-        url: "images/Btn_OtherButton_Polygon02.png"
+        url: `${process.env.REACT_APP_FRONT}images/Btn_OtherButton_Polygon02.png`
     },
     {
         id: 6,
-        url: "images/Girl and boy-eye.png"
+        url: `${process.env.REACT_APP_FRONT}images/Girl and boy-eye.png`
     },
     {
         id: 7,
-        url: "images/Girl-up.png"
+        url: `${process.env.REACT_APP_FRONT}images/Girl-up.png`
     },
     {
         id: 8,
-        url: "images/Girl and boy.png"
+        url: `${process.env.REACT_APP_FRONT}images/Girl and boy.png`
     },
     {
         id: 9,
-        url: "images/Girl-back.png"
+        url: `${process.env.REACT_APP_FRONT}images/Girl-back.png`
     },
     {
         id: 10,
-        url: "images/time.png"
+        url: `${process.env.REACT_APP_FRONT}images/time.png`
     },
     {
         id: 11,
-        url: "images/student.png"
+        url: `${process.env.REACT_APP_FRONT}images/student.png`
     },
     {
         id: 12,
-        url: "images/student-eye.png"
+        url: `${process.env.REACT_APP_FRONT}images/student-eye.png`
     },
     {
         id: 13,
-        url: "images/student girl.png"
+        url: `${process.env.REACT_APP_FRONT}images/student girl.png`
     },
     {
         id: 14,
-        url: "images/student girl - click.png"
+        url: `${process.env.REACT_APP_FRONT}images/student girl - click.png`
     }
 ]
